0bd992fcaa6059d1109cb3568f86d80404f49dc4
Serg <rumabasy@gmail.com>
2021-03-24 17:24:25 +0300

    modified 2

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.body.innerHTML='<h1>Hello from JavaScript</h1>'
-<<<<<<< HEAD
 console.log('New Test 222222222')
-=======
- console.log('Test git add .1111111git push!!')
->>>>>>> 5191473fa1a4497b14752035f73dcf3ee002e817
+console.log('Test git add .1111111git push!!')
+alert("yes")
  
